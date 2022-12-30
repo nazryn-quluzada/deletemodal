df59ad2e4e5aa9dafe623afa160d608b12db33fe
nazryn-quluzada <121688142+nazryn-quluzada@users.noreply.github.com>
2022-12-30 21:37:55 +0300

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,19 @@
+function myFun(){
+    document.querySelector('.modal-content').style.display = 'block';
+}
+
+myFun()
+
+function secondFun(){
+    document.querySelector('.modal-content').style.display = 'none';
+
+}
+
+secondFun()
+
+function thirdFun(){
+   document.querySelector('.modal-content').style.display = 'none'
+
+}
+
+thirdFun()
